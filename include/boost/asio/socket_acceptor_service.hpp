bb236cb7b3c079f0327949d992ba8c84580dbdbc
Christopher Kohlhoff <chris@kohlhoff.com>
2007-02-09 06:41:29 +0000

    Add is_open() function to acceptor.
    [SVN r36915]

--- include/boost/asio/socket_acceptor_service.hpp
+++ include/boost/asio/socket_acceptor_service.hpp
@@ -121,6 +121,12 @@ public:
     return service_impl_.assign(impl, protocol, native_acceptor, ec);
   }
 
+  /// Determine whether the acceptor is open.
+  bool is_open(const implementation_type& impl) const
+  {
+    return service_impl_.is_open(impl);
+  }
+
   /// Cancel all asynchronous operations associated with the acceptor.
   boost::system::error_code cancel(implementation_type& impl,
       boost::system::error_code& ec)
