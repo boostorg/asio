bd14eccee76cd7efd81adc325218a71e8414be9f
Christopher Kohlhoff <chris@kohlhoff.com>
2007-02-20 13:18:37 +0000

    Fix error_code output.
    [SVN r37019]

--- example/services/daytime_client.cpp
+++ example/services/daytime_client.cpp
@@ -51,7 +51,7 @@ void connect_handler(const boost::system::error_code& e, debug_stream_socket* s,
   }
   else
   {
-    std::cerr << e << std::endl;
+    std::cerr << e.message() << std::endl;
   }
 }
 
