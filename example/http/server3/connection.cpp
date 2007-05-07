5f61b15b8d584f54c4fff596347f5ace52aa3457
Christopher Kohlhoff <chris@kohlhoff.com>
2007-05-07 11:46:23 +0000

    Add new HTTP server examples illustrating io_service-per-cpu and single-io_service-multiple-threads designs.
    [SVN r37612]

---
+++ example/http/server3/connection.cpp
@@ -0,0 +1,95 @@
+//
+// connection.cpp
+// ~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2007 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "connection.hpp"
+#include <vector>
+#include <boost/bind.hpp>
+#include "request_handler.hpp"
+
+namespace http {
+namespace server3 {
+
+connection::connection(boost::asio::io_service& io_service,
+    request_handler& handler)
+  : strand_(io_service),
+    socket_(io_service),
+    request_handler_(handler)
+{
+}
+
+boost::asio::ip::tcp::socket& connection::socket()
+{
+  return socket_;
+}
+
+void connection::start()
+{
+  socket_.async_read_some(boost::asio::buffer(buffer_),
+      strand_.wrap(
+        boost::bind(&connection::handle_read, shared_from_this(),
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred)));
+}
+
+void connection::stop()
+{
+  socket_.close();
+}
+
+void connection::handle_read(const boost::system::error_code& e,
+    std::size_t bytes_transferred)
+{
+  if (!e)
+  {
+    boost::tribool result;
+    boost::tie(result, boost::tuples::ignore) = request_parser_.parse(
+        request_, buffer_.data(), buffer_.data() + bytes_transferred);
+
+    if (result)
+    {
+      request_handler_.handle_request(request_, reply_);
+      boost::asio::async_write(socket_, reply_.to_buffers(),
+          strand_.wrap(
+            boost::bind(&connection::handle_write, shared_from_this(),
+              boost::asio::placeholders::error)));
+    }
+    else if (!result)
+    {
+      reply_ = reply::stock_reply(reply::bad_request);
+      boost::asio::async_write(socket_, reply_.to_buffers(),
+          strand_.wrap(
+            boost::bind(&connection::handle_write, shared_from_this(),
+              boost::asio::placeholders::error)));
+    }
+    else
+    {
+      socket_.async_read_some(boost::asio::buffer(buffer_),
+          strand_.wrap(
+            boost::bind(&connection::handle_read, shared_from_this(),
+              boost::asio::placeholders::error,
+              boost::asio::placeholders::bytes_transferred)));
+    }
+  }
+  else if (e != boost::asio::error::operation_aborted)
+  {
+    stop();
+  }
+}
+
+void connection::handle_write(const boost::system::error_code& e)
+{
+  if (e != boost::asio::error::operation_aborted)
+  {
+    stop();
+  }
+}
+
+} // namespace server3
+} // namespace http
