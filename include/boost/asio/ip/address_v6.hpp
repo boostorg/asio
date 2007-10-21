420ddcf65521a6821c9eb4b8ae3b9cd15fc35237
Daniel James <daniel@calamity.org.uk>
2007-10-21 08:54:36 +0000

    Merge from trunk. Including floating point hash tweaks.
    [SVN r40266]

--- include/boost/asio/ip/address_v6.hpp
+++ include/boost/asio/ip/address_v6.hpp
@@ -301,7 +301,7 @@ public:
   {
     using namespace std; // For memcmp.
     int memcmp_result = memcmp(&a1.addr_, &a2.addr_,
-        sizeof(boost::asio::detail::in6_addr_type)) < 0;
+        sizeof(boost::asio::detail::in6_addr_type));
     if (memcmp_result < 0)
       return true;
     if (memcmp_result > 0)
