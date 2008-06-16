1539f2c8a19068c7baa0526947d65817c917aaec
Christopher Kohlhoff <chris@kohlhoff.com>
2008-06-16 00:41:29 +0000

    Add an iterator for bytewise traversal of a buffer sequence.
    [SVN r46415]

--- include/boost/asio.hpp
+++ include/boost/asio.hpp
@@ -34,6 +34,7 @@
 #include <boost/asio/buffered_stream.hpp>
 #include <boost/asio/buffered_write_stream_fwd.hpp>
 #include <boost/asio/buffered_write_stream.hpp>
+#include <boost/asio/buffers_iterator.hpp>
 #include <boost/asio/completion_condition.hpp>
 #include <boost/asio/datagram_socket_service.hpp>
 #include <boost/asio/deadline_timer_service.hpp>
