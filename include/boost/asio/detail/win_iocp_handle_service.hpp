b79f9a65cd764141c34ede2f45357234f4ea11f6
Christopher Kohlhoff <chris@kohlhoff.com>
2009-04-16 12:31:27 +0000

    Specifically handle the case when an overlapped ReadFile call fails with ERROR_MORE_DATA. This enables a hack where a windows::stream_handle can be used with a message-oriented named pipe.
    [SVN r52419]

--- include/boost/asio/detail/win_iocp_handle_service.hpp
+++ include/boost/asio/detail/win_iocp_handle_service.hpp
@@ -573,7 +573,7 @@ public:
     if (!ok) 
     {
       DWORD last_error = ::GetLastError();
-      if (last_error != ERROR_IO_PENDING)
+      if (last_error != ERROR_IO_PENDING && last_error != ERROR_MORE_DATA)
       {
         if (last_error == ERROR_HANDLE_EOF)
         {
@@ -763,7 +763,7 @@ public:
         static_cast<DWORD>(boost::asio::buffer_size(buffer)),
         &bytes_transferred, ptr.get());
     DWORD last_error = ::GetLastError();
-    if (!ok && last_error != ERROR_IO_PENDING)
+    if (!ok && last_error != ERROR_IO_PENDING && last_error != ERROR_MORE_DATA)
     {
       boost::asio::io_service::work work(this->get_io_service());
       ptr.reset();
