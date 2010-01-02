cf8c031f87823a6de1ae81d5fe320a5f9e61285b
Christopher Kohlhoff <chris@kohlhoff.com>
2010-01-02 09:48:01 +0000

    Don't block signals while performing system calls, but instead restart the calls if they are interrupted.
    [SVN r58628]

--- include/boost/asio/detail/reactive_descriptor_service.hpp
+++ include/boost/asio/detail/reactive_descriptor_service.hpp
@@ -348,16 +348,23 @@ public:
             boost::asio::buffer_size(buffer));
       }
 
-      // Write the data.
-      int bytes = descriptor_ops::gather_write(descriptor_, bufs, i, ec);
+      for (;;)
+      {
+        // Write the data.
+        int bytes = descriptor_ops::gather_write(descriptor_, bufs, i, ec);
 
-      // Check if we need to run the operation again.
-      if (ec == boost::asio::error::would_block
-          || ec == boost::asio::error::try_again)
-        return false;
+        // Retry operation if interrupted by signal.
+        if (ec == boost::asio::error::interrupted)
+          continue;
 
-      bytes_transferred = (bytes < 0 ? 0 : bytes);
-      return true;
+        // Check if we need to run the operation again.
+        if (ec == boost::asio::error::would_block
+            || ec == boost::asio::error::try_again)
+          return false;
+
+        bytes_transferred = (bytes < 0 ? 0 : bytes);
+        return true;
+      }
     }
 
     void complete(const boost::system::error_code& ec,
@@ -600,18 +607,25 @@ public:
             boost::asio::buffer_size(buffer));
       }
 
-      // Read some data.
-      int bytes = descriptor_ops::scatter_read(descriptor_, bufs, i, ec);
-      if (bytes == 0)
-        ec = boost::asio::error::eof;
+      for (;;)
+      {
+        // Read some data.
+        int bytes = descriptor_ops::scatter_read(descriptor_, bufs, i, ec);
+        if (bytes == 0)
+          ec = boost::asio::error::eof;
 
-      // Check if we need to run the operation again.
-      if (ec == boost::asio::error::would_block
-          || ec == boost::asio::error::try_again)
-        return false;
+        // Retry operation if interrupted by signal.
+        if (ec == boost::asio::error::interrupted)
+          continue;
 
-      bytes_transferred = (bytes < 0 ? 0 : bytes);
-      return true;
+        // Check if we need to run the operation again.
+        if (ec == boost::asio::error::would_block
+            || ec == boost::asio::error::try_again)
+          return false;
+
+        bytes_transferred = (bytes < 0 ? 0 : bytes);
+        return true;
+      }
     }
 
     void complete(const boost::system::error_code& ec,
