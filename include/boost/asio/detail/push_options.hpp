60724f91abc48aee55b4f345b66cafce44a78793
Christopher Kohlhoff <chris@kohlhoff.com>
2010-11-04 11:40:42 +0000

    MacPorts gcc also defines __APPLE_CC__, but to 1.
    [SVN r66398]

--- include/boost/asio/detail/push_options.hpp
+++ include/boost/asio/detail/push_options.hpp
@@ -31,12 +31,14 @@
 #  pragma pack (push, 8)
 # endif
 
-# if defined(__OBJC__) && !defined(__APPLE_CC__)
-#  if !defined(BOOST_ASIO_DISABLE_OBJC_WORKAROUND)
-#   if !defined(Protocol) && !defined(id)
-#    define Protocol cpp_Protocol
-#    define id cpp_id
-#    define BOOST_ASIO_OBJC_WORKAROUND
+# if defined(__OBJC__)
+#  if !defined(__APPLE_CC__) || (__APPLE_CC__ <= 1)
+#   if !defined(BOOST_ASIO_DISABLE_OBJC_WORKAROUND)
+#    if !defined(Protocol) && !defined(id)
+#     define Protocol cpp_Protocol
+#     define id cpp_id
+#     define BOOST_ASIO_OBJC_WORKAROUND
+#    endif
 #   endif
 #  endif
 # endif
