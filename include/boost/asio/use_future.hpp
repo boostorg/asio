546362b4252f2e553c4c9d7c37bd26840a608bcc
Christopher Kohlhoff <chris@kohlhoff.com>
2013-05-17 02:35:08 +0000

    Add the asio::use_future special value, which adds first-class support for returning a C++11 std::future from an asynchronous operation's initiating function.
    To use asio::use_future, pass it to an asynchronous operation instead of
    a normal completion handler. For example:
    
      std::future<std::size_t> length =
        my_socket.async_read_some(my_buffer, asio::use_future);
    
    Where a completion handler signature has the form:
    
      void handler(error_code ec, result_type result);
    
    the initiating function returns a std::future templated on result_type.
    In the above example, this is std::size_t. If the asynchronous operation
    fails, the error_code is converted into a system_error exception and
    passed back to the caller through the future.
    
    Where a completion handler signature has the form:
    
      void handler(error_code ec);
    
    the initiating function returns std::future<void>. As above, an error
    is passed back in the future as a system_error exception.
    
    
    [SVN r84313]

---
+++ include/boost/asio/use_future.hpp
@@ -0,0 +1,66 @@
+//
+// use_future.hpp
+// ~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef BOOST_ASIO_USE_FUTURE_HPP
+#define BOOST_ASIO_USE_FUTURE_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include <boost/asio/detail/config.hpp>
+#include <memory>
+
+#include <boost/asio/detail/push_options.hpp>
+
+namespace boost {
+namespace asio {
+
+template <typename Allocator = std::allocator<void> >
+class use_future_t
+{
+public:
+  typedef Allocator allocator_type;
+
+  constexpr use_future_t()
+  {
+  }
+
+  explicit use_future_t(const Allocator& allocator)
+    : allocator_(allocator)
+  {
+  }
+
+  template <typename OtherAllocator>
+  use_future_t<OtherAllocator> operator[](const OtherAllocator& allocator) const
+  {
+    return use_future_t<OtherAllocator>(allocator);
+  }
+
+  allocator_type get_allocator() const
+  {
+    return allocator_;
+  }
+
+private:
+  Allocator allocator_;
+};
+
+// A special value, similar to std::nothrow.
+constexpr use_future_t<> use_future;
+
+} // namespace asio
+} // namespace boost
+
+#include <boost/asio/detail/pop_options.hpp>
+
+#include <boost/asio/impl/use_future.hpp>
+
+#endif // BOOST_ASIO_USE_FUTURE_HPP
