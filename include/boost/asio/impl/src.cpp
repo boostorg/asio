642a1cf824ff8d8250062e10ded510935a7752e9
Christopher Kohlhoff <chris@kohlhoff.com>
2013-05-25 11:57:36 +0000

    Fix problem in #warning directive. Refs #7939
    [SVN r84488]

--- include/boost/asio/impl/src.cpp
+++ include/boost/asio/impl/src.cpp
@@ -13,13 +13,13 @@
   || defined(__DMC__)
 # pragma message ( \
     "This file is deprecated. " \
-    "Please #include <boost/asio/impl/src.hpp> instead.>)
+    "Please #include <boost/asio/impl/src.hpp> instead.")
 #elif defined(__GNUC__) \
   || defined(__HP_aCC) \
   || defined(__SUNPRO_CC) \
   || defined(__IBMCPP__)
 # warning "This file is deprecated."
-# warning "Please #include <boost/asio/impl/src.hpp> instead.>
+# warning "Please #include <boost/asio/impl/src.hpp> instead."
 #endif
 
 #include <boost/asio/impl/src.hpp>
