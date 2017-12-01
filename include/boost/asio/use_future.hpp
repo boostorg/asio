dfa54b4fc5955116913c230d13209dd408f50104
Christopher Kohlhoff <chris@kohlhoff.com>
2017-12-02 10:09:55 +1100

    Suppress MSVC dynamic initialisation warning.

--- include/boost/asio/use_future.hpp
+++ include/boost/asio/use_future.hpp
@@ -127,7 +127,10 @@ private:
  * See the documentation for boost::asio::use_future_t for a usage example.
  */
 #if defined(BOOST_ASIO_MSVC)
+# pragma warning (push)
+# pragma warning (disable:4592)
 __declspec(selectany) use_future_t<> use_future;
+# pragma warning (pop)
 #elif defined(BOOST_ASIO_HAS_CONSTEXPR) || defined(GENERATING_DOCUMENTATION)
 constexpr use_future_t<> use_future;
 #endif
