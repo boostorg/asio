9f2516f590149bf883b67d031eb2000c388c24b3
Jan Beich <jbeich@FreeBSD.org>
2018-08-06 13:59:09 +0000

    Unbreak build with Clang/libc++ 7.0.0
    $ echo '#include <boost/asio.hpp>' | clang -xc++ -c - -isystem/usr/local/include
    In file included from <stdin>:1:
    In file included from /usr/local/include/boost/asio.hpp:23:
    In file included from /usr/local/include/boost/asio/basic_datagram_socket.hpp:20:
    In file included from /usr/local/include/boost/asio/basic_socket.hpp:40:
    In file included from /usr/local/include/boost/asio/detail/reactive_socket_service.hpp:22:
    In file included from /usr/local/include/boost/asio/buffer.hpp:27:
    In file included from /usr/local/include/boost/asio/detail/string_view.hpp:25:
    /usr/include/c++/v1/experimental/string_view:11:2: error: "<experimental/string_view> has been removed. Use
          <string_view> instead."

--- include/boost/asio/detail/config.hpp
+++ include/boost/asio/detail/config.hpp
@@ -777,7 +777,7 @@
 #if !defined(BOOST_ASIO_HAS_STD_STRING_VIEW)
 # if !defined(BOOST_ASIO_DISABLE_STD_STRING_VIEW)
 #  if defined(__clang__)
-#   if (__cplusplus >= 201703)
+#   if defined(BOOST_ASIO_HAS_CLANG_LIBCXX) || (__cplusplus >= 201703)
 #    if __has_include(<string_view>)
 #     define BOOST_ASIO_HAS_STD_STRING_VIEW 1
 #    endif // __has_include(<string_view>)
