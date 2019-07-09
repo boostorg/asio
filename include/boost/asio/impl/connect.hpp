1e3eea834d357cdf4449dcefb59669191168fbab
chris.kohlhoff <chris.kohlhoff@clearpool.io>
2019-07-09 19:00:05 +1000

    Annotate case fall-through in connect() implementation.

--- include/boost/asio/impl/connect.hpp
+++ include/boost/asio/impl/connect.hpp
@@ -372,7 +372,7 @@ namespace detail
             return;
           }
 
-          default:
+          /* fall-through */ default:
 
           if (iter == end)
             break;
@@ -542,7 +542,7 @@ namespace detail
             return;
           }
 
-          default:
+          /* fall-through */ default:
 
           if (iter_ == end_)
             break;
