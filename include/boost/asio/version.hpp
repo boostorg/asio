5dbdd5c48c6e0fcdb83ac2b5066923b64f5d26a9
Christopher Kohlhoff <chris@kohlhoff.com>
2019-08-06 19:21:34 +1000

    Version bump.

--- include/boost/asio/version.hpp
+++ include/boost/asio/version.hpp
@@ -18,6 +18,6 @@
 // BOOST_ASIO_VERSION % 100 is the sub-minor version
 // BOOST_ASIO_VERSION / 100 % 1000 is the minor version
 // BOOST_ASIO_VERSION / 100000 is the major version
-#define BOOST_ASIO_VERSION 101400 // 1.14.0
+#define BOOST_ASIO_VERSION 101401 // 1.14.1
 
 #endif // BOOST_ASIO_VERSION_HPP
