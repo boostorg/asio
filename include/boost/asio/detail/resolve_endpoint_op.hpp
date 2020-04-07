df4d3c18fd73877491c5d814828fb280367eb96c
Christopher Kohlhoff <chris@kohlhoff.com>
2020-04-07 11:20:20 +1000

    Fix work counting issue in asynchronous resolver endpoint implementation.

--- include/boost/asio/detail/resolve_endpoint_op.hpp
+++ include/boost/asio/detail/resolve_endpoint_op.hpp
@@ -73,7 +73,6 @@ public:
     // Take ownership of the operation object.
     resolve_endpoint_op* o(static_cast<resolve_endpoint_op*>(base));
     ptr p = { boost::asio::detail::addressof(o->handler_), o, o };
-    handler_work<Handler, IoExecutor> w(o->handler_, o->io_executor_);
 
     if (owner && owner != &o->scheduler_)
     {
@@ -96,6 +95,9 @@ public:
     {
       // The operation has been returned to the main io_context. The completion
       // handler is ready to be delivered.
+
+      // Take ownership of the operation's outstanding work.
+      handler_work<Handler, IoExecutor> w(o->handler_, o->io_executor_);
 
       BOOST_ASIO_HANDLER_COMPLETION((*o));
 
