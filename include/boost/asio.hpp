ac7e13f51bbbc75eaa64cb41b99206116ec89b7f
Christopher Kohlhoff <chris@kohlhoff.com>
2020-06-22 23:03:15 +1000

    Add execution::context_as property adapter.

--- include/boost/asio.hpp
+++ include/boost/asio.hpp
@@ -60,6 +60,7 @@
 #include <boost/asio/execution/blocking_adaptation.hpp>
 #include <boost/asio/execution/bulk_guarantee.hpp>
 #include <boost/asio/execution/context.hpp>
+#include <boost/asio/execution/context_as.hpp>
 #include <boost/asio/execution/execute.hpp>
 #include <boost/asio/execution/executor.hpp>
 #include <boost/asio/execution/invocable_archetype.hpp>
