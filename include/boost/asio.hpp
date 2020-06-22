f6cfbc3982c72dd8afd5e99fb57cd7816569d2ff
Christopher Kohlhoff <chris@kohlhoff.com>
2020-06-22 22:49:58 +1000

    Add execution::execute() customisation point object.

--- include/boost/asio.hpp
+++ include/boost/asio.hpp
@@ -54,6 +54,7 @@
 #include <boost/asio/detached.hpp>
 #include <boost/asio/dispatch.hpp>
 #include <boost/asio/error.hpp>
+#include <boost/asio/execution/execute.hpp>
 #include <boost/asio/execution/invocable_archetype.hpp>
 #include <boost/asio/execution_context.hpp>
 #include <boost/asio/executor.hpp>
