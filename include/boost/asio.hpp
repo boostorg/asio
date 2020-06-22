834c5bdc8a573a44d140c8d699ba0355c7ffccfd
Christopher Kohlhoff <chris@kohlhoff.com>
2020-06-22 22:50:34 +1000

    Add execution::executor concept and execution::is_executor trait.

--- include/boost/asio.hpp
+++ include/boost/asio.hpp
@@ -55,6 +55,7 @@
 #include <boost/asio/dispatch.hpp>
 #include <boost/asio/error.hpp>
 #include <boost/asio/execution/execute.hpp>
+#include <boost/asio/execution/executor.hpp>
 #include <boost/asio/execution/invocable_archetype.hpp>
 #include <boost/asio/execution_context.hpp>
 #include <boost/asio/executor.hpp>
