df02a6681122726658541739133344fd0c5b1317
Christopher Kohlhoff <chris@kohlhoff.com>
2020-11-02 13:05:46 +1100

    More support for detecting handlers that have no custom executor.

--- include/boost/asio/detail/bind_handler.hpp
+++ include/boost/asio/detail/bind_handler.hpp
@@ -853,6 +853,7 @@ struct associated_allocator<detail::binder2<Handler, Arg1, Arg2>, Allocator>
 
 template <typename Handler, typename Arg1, typename Executor>
 struct associated_executor<detail::binder1<Handler, Arg1>, Executor>
+  : detail::associated_executor_forwarding_base<Handler, Executor>
 {
   typedef typename associated_executor<Handler, Executor>::type type;
 
@@ -865,6 +866,7 @@ struct associated_executor<detail::binder1<Handler, Arg1>, Executor>
 
 template <typename Handler, typename Arg1, typename Arg2, typename Executor>
 struct associated_executor<detail::binder2<Handler, Arg1, Arg2>, Executor>
+  : detail::associated_executor_forwarding_base<Handler, Executor>
 {
   typedef typename associated_executor<Handler, Executor>::type type;
 
@@ -904,6 +906,7 @@ struct associated_allocator<
 
 template <typename Handler, typename Arg1, typename Executor>
 struct associated_executor<detail::move_binder1<Handler, Arg1>, Executor>
+  : detail::associated_executor_forwarding_base<Handler, Executor>
 {
   typedef typename associated_executor<Handler, Executor>::type type;
 
@@ -916,6 +919,7 @@ struct associated_executor<detail::move_binder1<Handler, Arg1>, Executor>
 
 template <typename Handler, typename Arg1, typename Arg2, typename Executor>
 struct associated_executor<detail::move_binder2<Handler, Arg1, Arg2>, Executor>
+  : detail::associated_executor_forwarding_base<Handler, Executor>
 {
   typedef typename associated_executor<Handler, Executor>::type type;
 
