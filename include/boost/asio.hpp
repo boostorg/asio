6dc719f0367c914c3b25444184c41ed1d95523ca
Christopher Kohlhoff <chris@kohlhoff.com>
2021-06-28 10:08:23 +1000

    Add associated_cancellation_slot associator.

--- include/boost/asio.hpp
+++ include/boost/asio.hpp
@@ -19,6 +19,7 @@
 
 #include <boost/asio/associated_allocator.hpp>
 #include <boost/asio/associated_executor.hpp>
+#include <boost/asio/associated_cancellation_slot.hpp>
 #include <boost/asio/associator.hpp>
 #include <boost/asio/async_result.hpp>
 #include <boost/asio/awaitable.hpp>
