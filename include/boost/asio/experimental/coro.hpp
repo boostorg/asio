77cf53057c9cb69667aa8bb4ea1978a917cf9966
Christopher Kohlhoff <chris@kohlhoff.com>
2021-07-04 13:09:14 +1000

    Fix return_void() in experimental::coro.

--- include/boost/asio/experimental/coro.hpp
+++ include/boost/asio/experimental/coro.hpp
@@ -853,7 +853,7 @@ struct coro_promise_exchange<Yield, Input, void> : coro_awaited_from
       std::exchange(awaited_from, noop_coroutine())};
   }
 
-  void return_void() {}
+  void return_void() { result_.reset(); }
 };
 
 template <typename Return>
@@ -898,7 +898,7 @@ struct coro_promise_exchange<Yield, void, void> : coro_awaited_from
     return yield_input<void>{std::exchange(awaited_from, noop_coroutine())};
   }
 
-  void return_void() {}
+  void return_void() { result_.reset(); }
 };
 
 template <typename Yield, typename Return, typename Executor>
