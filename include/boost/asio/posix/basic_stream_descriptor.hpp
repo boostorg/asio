0938b4e425dd3d24f42adc12080da35099b0df86
Christopher Kohlhoff <chris@kohlhoff.com>
2021-07-31 19:28:50 +1000

    Fix posix::basic_stream_descriptor move operations.
    They were only working when using the default executor.

--- include/boost/asio/posix/basic_stream_descriptor.hpp
+++ include/boost/asio/posix/basic_stream_descriptor.hpp
@@ -16,7 +16,7 @@
 #endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
 
 #include <boost/asio/detail/config.hpp>
-#include <boost/asio/posix/descriptor.hpp>
+#include <boost/asio/posix/basic_descriptor.hpp>
 
 #if defined(BOOST_ASIO_HAS_POSIX_STREAM_DESCRIPTOR) \
   || defined(GENERATING_DOCUMENTATION)
@@ -155,7 +155,7 @@ public:
    * constructor.
    */
   basic_stream_descriptor(basic_stream_descriptor&& other) BOOST_ASIO_NOEXCEPT
-    : descriptor(std::move(other))
+    : basic_descriptor<Executor>(std::move(other))
   {
   }
 
@@ -173,7 +173,7 @@ public:
    */
   basic_stream_descriptor& operator=(basic_stream_descriptor&& other)
   {
-    descriptor::operator=(std::move(other));
+    basic_descriptor<Executor>::operator=(std::move(other));
     return *this;
   }
 #endif // defined(BOOST_ASIO_HAS_MOVE) || defined(GENERATING_DOCUMENTATION)
