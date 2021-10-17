38e4883c211ea1192919fdc72b901d74f67743c5
Christopher Kohlhoff <chris@kohlhoff.com>
2021-10-17 11:03:31 +1100

    Fix typo in io_context documentation (overlaod -> overload).

--- include/boost/asio/io_context.hpp
+++ include/boost/asio/io_context.hpp
@@ -401,7 +401,7 @@ public:
   BOOST_ASIO_DECL count_type run_one();
 
 #if !defined(BOOST_ASIO_NO_DEPRECATED)
-  /// (Deprecated: Use non-error_code overlaod.) Run the io_context object's
+  /// (Deprecated: Use non-error_code overload.) Run the io_context object's
   /// event processing loop to execute at most one handler.
   /**
    * The run_one() function blocks until one handler has been dispatched, or
