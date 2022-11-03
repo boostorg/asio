6ab07fa35e11f622b0bfc80c25b3217292e0c170
Christopher Kohlhoff <chris@kohlhoff.com>
2022-11-03 17:32:07 +1100

    Add io_executor_type and get_io_executor to async_compose's 'self' object.

--- include/boost/asio/compose.hpp
+++ include/boost/asio/compose.hpp
@@ -72,11 +72,16 @@ public:
   }
 #endif // defined(BOOST_ASIO_HAS_MOVE)
 
-  typedef typename associated_executor<Handler,
-      typename composed_work_guard<
-        typename Work::head_type
-      >::executor_type
-    >::type executor_type;
+  typedef typename composed_work_guard<
+    typename Work::head_type>::executor_type io_executor_type;
+
+  io_executor_type get_io_executor() const BOOST_ASIO_NOEXCEPT
+  {
+    return work_.head_.get_executor();
+  }
+
+  typedef typename associated_executor<Handler, io_executor_type>::type
+    executor_type;
 
   executor_type get_executor() const BOOST_ASIO_NOEXCEPT
   {
