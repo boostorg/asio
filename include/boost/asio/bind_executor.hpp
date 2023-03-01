c35218f2198bb2c655b21b39b02ec73bac7b0ed4
Christopher Kohlhoff <chris@kohlhoff.com>
2023-03-01 23:06:21 +1100

    Change executor_binder to support detection of unspecialised associators.

--- include/boost/asio/bind_executor.hpp
+++ include/boost/asio/bind_executor.hpp
@@ -740,16 +740,17 @@ private:
 template <template <typename, typename> class Associator,
     typename T, typename Executor, typename DefaultCandidate>
 struct associator<Associator, executor_binder<T, Executor>, DefaultCandidate>
+  : Associator<T, DefaultCandidate>
 {
-  typedef typename Associator<T, DefaultCandidate>::type type;
-
-  static type get(const executor_binder<T, Executor>& b) BOOST_ASIO_NOEXCEPT
+  static typename Associator<T, DefaultCandidate>::type
+  get(const executor_binder<T, Executor>& b) BOOST_ASIO_NOEXCEPT
   {
     return Associator<T, DefaultCandidate>::get(b.get());
   }
 
-  static BOOST_ASIO_AUTO_RETURN_TYPE_PREFIX(type) get(
-      const executor_binder<T, Executor>& b,
+  static BOOST_ASIO_AUTO_RETURN_TYPE_PREFIX2(
+      typename Associator<T, DefaultCandidate>::type)
+  get(const executor_binder<T, Executor>& b,
       const DefaultCandidate& c) BOOST_ASIO_NOEXCEPT
     BOOST_ASIO_AUTO_RETURN_TYPE_SUFFIX((
       Associator<T, DefaultCandidate>::get(b.get(), c)))
