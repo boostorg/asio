89ed2b6a47e6776426f68bb08b93c8fb25a13eae
Christopher Kohlhoff <chris@kohlhoff.com>
2024-10-23 21:17:04 +1100

    Remove deprecated alias experimental::prepend.

--- include/boost/asio/experimental/co_spawn.hpp
+++ include/boost/asio/experimental/co_spawn.hpp
@@ -21,7 +21,7 @@
 #include <boost/asio/detail/type_traits.hpp>
 #include <boost/asio/experimental/coro.hpp>
 #include <boost/asio/experimental/deferred.hpp>
-#include <boost/asio/experimental/prepend.hpp>
+#include <boost/asio/prepend.hpp>
 #include <boost/asio/redirect_error.hpp>
 
 #include <boost/asio/detail/push_options.hpp>
