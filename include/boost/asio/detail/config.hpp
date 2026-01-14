boostorg/asio

commit 8b80ad7f2447cdbfc99f02a89a75d424dc8f9910

Update detection of std::aligned_alloc when using libc++.
Newer libc++ versions no longer define _LIBCPP_HAS_ALIGNED_ALLOC,
so instead perform explicit platform detection.

include/boost/asio/detail/config.hpp
@@ -373,10 +373,20 @@
 #  if (__cplusplus >= 201703)
 #   if defined(__clang__)
 #    if defined(BOOST_ASIO_HAS_CLANG_LIBCXX)
-#     if (_LIBCPP_STD_VER > 14) && defined(_LIBCPP_HAS_ALIGNED_ALLOC) \
-        && !defined(_LIBCPP_MSVCRT) && !defined(__MINGW32__)
-#      if defined(__ANDROID__) && (__ANDROID_API__ >= 28)
+#     if (_LIBCPP_STD_VER > 14)
+#      if defined(__FreeBSD__) || defined(__Fuchsia__) || defined(__wasi__) \
+         || defined(__NetBSD__) || defined(__OpenBSD__)
+#       define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
+#      elif defined(__linux__)
+#       if defined(_LIBCPP_HAS_MUSL_LIBC)
 #        define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
+#       else // !defined(_LIBCPP_HAS_MUSL_LIBC)
+#        if (__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 17)
+#         define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
+#        endif // (__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 17)
+#       endif // !defined(_LIBCPP_HAS_MUSL_LIBC)
+#      elif defined(__ANDROID__) && (__ANDROID_API__ >= 28)
+#       define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
 #      elif defined(__APPLE__)
 #       if defined(__MAC_OS_X_VERSION_MIN_REQUIRED)
 #        if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
@@ -395,11 +405,8 @@
 #         define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
 #        endif // (__WATCH_OS_VERSION_MIN_REQUIRED >= 60000)
 #       endif // defined(__WATCH_OS_X_VERSION_MIN_REQUIRED)
-#      else // defined(__APPLE__)
-#       define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
 #      endif // defined(__APPLE__)
-#     endif // (_LIBCPP_STD_VER > 14) && defined(_LIBCPP_HAS_ALIGNED_ALLOC)
-            //   && !defined(_LIBCPP_MSVCRT) && !defined(__MINGW32__)
+#     endif // (_LIBCPP_STD_VER > 14)
 #    elif defined(_GLIBCXX_HAVE_ALIGNED_ALLOC)
 #     define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
 #    endif // defined(_GLIBCXX_HAVE_ALIGNED_ALLOC)

commit 72ed9cbc4bde511cdfaa01fe107c9b89bf0c7288

Android: fix compile break with musl
Fix a compile break when building with _LIBCPP_HAS_MUSL_LIBC and
an Android API older then 28

On Android both __linux__ and __ANDROID__ are defined. First check for
__ANDROID__ to get the correct behavior and not run into the Linux case

Compile break was introduced with
8b80ad7f2447cdbfc99f02a89a75d424dc8f9910

include/boost/asio/detail/config.hpp
@@ -377,6 +377,10 @@
 #      if defined(__FreeBSD__) || defined(__Fuchsia__) || defined(__wasi__) \
          || defined(__NetBSD__) || defined(__OpenBSD__)
 #       define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
+#      elif defined(__ANDROID__) 
+#       if (__ANDROID_API__ >= 28)
+#         define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
+#       endif // (__ANDROID_API__ >= 28)
 #      elif defined(__linux__)
 #       if defined(_LIBCPP_HAS_MUSL_LIBC)
 #        define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
@@ -385,8 +389,6 @@
 #         define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
 #        endif // (__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 17)
 #       endif // !defined(_LIBCPP_HAS_MUSL_LIBC)
-#      elif defined(__ANDROID__) && (__ANDROID_API__ >= 28)
-#       define BOOST_ASIO_HAS_STD_ALIGNED_ALLOC 1
 #      elif defined(__APPLE__)
 #       if defined(__MAC_OS_X_VERSION_MIN_REQUIRED)
 #        if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)