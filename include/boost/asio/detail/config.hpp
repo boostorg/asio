dc3f788435be24058b768a68bdec5135ade09e19
Christopher Kohlhoff <chris@kohlhoff.com>
2026-03-03 20:49:49 +1100

    Fix MSG_NOSIGNAL handling on NetBSD.
    In /usr/include/sys/unistd.h:62, `_POSIX_VERSION' is only `200112L',
    but NetBSD supports MSG_NOSIGNAL anyway.

--- include/boost/asio/detail/config.hpp
+++ include/boost/asio/detail/config.hpp
@@ -1473,7 +1473,7 @@
 
 // Kernel support for MSG_NOSIGNAL.
 #if !defined(BOOST_ASIO_HAS_MSG_NOSIGNAL)
-# if defined(__linux__)
+# if defined(__linux__) || defined(__NetBSD__)
 #  define BOOST_ASIO_HAS_MSG_NOSIGNAL 1
 # elif defined(_POSIX_VERSION)
 #  if (_POSIX_VERSION >= 200809L)
