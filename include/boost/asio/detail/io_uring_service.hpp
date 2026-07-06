ff5a0a5f5cb783b8f19f352fff009296aa6b91f3
Christopher Kohlhoff <chris@kohlhoff.com>
2026-07-06 23:55:06 +1000

    Add "reactor" / "io_uring_ring_size" (unsigned int) config parameter.
    This parameter specifies the number of entries used to size each
    io_uring instance (passed to io_uring_queue_init). Defaults to 16384.
    Specifying smaller values reduces the locked-memory footprint of each
    ring.

--- include/boost/asio/detail/io_uring_service.hpp
+++ include/boost/asio/detail/io_uring_service.hpp
@@ -207,8 +207,10 @@ public:
   BOOST_ASIO_DECL void interrupt();
 
 private:
-  // The hint to pass to io_uring_queue_init to size its data structures.
-  enum { ring_size = 16384 };
+  // The default hint to pass to io_uring_queue_init to size its data
+  // structures, used when the "reactor" / "io_uring_ring_size" configuration
+  // value is not set.
+  enum { default_ring_size = 16384 };
 
   // The number of operations to complete in a batch.
   enum { complete_batch_size = 128 };
@@ -316,6 +318,9 @@ private:
 
   // The number of operations to submit in a batch.
   const int submit_batch_size_;
+
+  // The number of entries used to size each io_uring.
+  const unsigned int ring_size_;
 
   // A count of SQE submissions that are yet to be flushed.
   atomic_count unflushed_submits_;
